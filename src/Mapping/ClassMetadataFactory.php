816cbc2be19c47d0f0edc266264fb0b1611b9f18
Saidou GUEYE <saidou.gueye@gmail.com>
2018-04-16 16:00:33 -0400

    Test handled class metadata creation

--- src/Mapping/ClassMetadataFactory.php
+++ src/Mapping/ClassMetadataFactory.php
@@ -52,17 +52,71 @@ class ClassMetadataFactory implements ClassMetadataFactoryInterface
     }
 
     /**
+     * @param string $className
+     * @return string
+     */
+    private function getClassCacheKey($className)
+    {
+        return str_replace('\\', '-', $className);
+    }
+
+    /**
+     * @param $className
+     * @return ClassMetadata
+     */
+    private function newClassMetadata($className)
+    {
+        return new ClassMetadata($className);
+    }
+
+    /**
+     * @param $className
+     * @return array
+     * @throws MappingException
+     */
+    private function loadMetadata($className)
+    {
+        if(!$this->driver->handlesClass($className)) {
+            throw new MappingException("Class '$className'  not handled by driver");
+        }
+        $metadata = $this->newClassMetadata($className);
+        $this->loadedMetadata[$className] = $metadata;
+        $this->driver->loadMetadataForClass($className, $metadata);
+        return [$className];
+    }
+
+    /**
      * Gets the class metadata descriptor for a class.
      *
      * @param string $className The name of the class.
      *
      * @return ClassMetadataInterface
+     * @throws MappingException
      */
     public function getMetadataFor($className)
     {
         if (isset($this->loadedMetadata[$className])) {
             return $this->loadedMetadata[$className];
         }
+
+        $cacheItemKey = $this->getClassCacheKey($className);
+        $cacheItem = $this->cache->getItem($cacheItemKey);
+        if($cacheItem->isHit()) {
+            $this->loadedMetadata[$className] = $cacheItem->get();
+        } else {
+            foreach ($this->loadMetadata($className) as $loadedClassName) {
+                $loadCacheItemKey = $this->getClassCacheKey($loadedClassName);
+                $loadedCacheItem = $this->cache->getItem($loadCacheItemKey);
+                if(!$loadedCacheItem->isHit()) {
+                    $loadedCacheItem->set($this->loadedMetadata[$loadedClassName]);
+                    $this->cache->save($loadedCacheItem);
+                }
+            }
+        }
+        if(!isset($this->loadedMetadata[$className])) {
+            throw new MappingException("Cannot load metadata for class '$className'");
+        }
+        return $this->loadedMetadata[$className];
     }
 
     /**
--- tests/Mapping/ClassMetadataFactoryTest.php
+++ tests/Mapping/ClassMetadataFactoryTest.php
@@ -9,33 +9,87 @@
 namespace Seydu\EloquentMetadata\Tests;
 
 
+use Cache\Adapter\Void\VoidCachePool;
 use PHPUnit\Framework\TestCase;
 use Psr\Cache\CacheItemPoolInterface;
+use Seydu\EloquentMetadata\Mapping\ArrayDriver;
 use Seydu\EloquentMetadata\Mapping\ClassMetadata;
 use Seydu\EloquentMetadata\Mapping\ClassMetadataFactory;
 use Seydu\EloquentMetadata\Mapping\DriverInterface;
+use Seydu\EloquentMetadata\Mapping\MappingException;
 use Seydu\EloquentMetadata\Tests\Models\Comment;
-use Seydu\EloquentMetadata\Tests\Models\Model;
 use Seydu\EloquentMetadata\Tests\Models\Post;
 
 class ClassMetadataFactoryTest extends TestCase
 {
+    private function getFixturesData()
+    {
+        return [
+            Post::class => [
+                'table' => 'post',
+                'fields' => [
+                    [
+                        'fieldName' => 'id',
+                        'columnName' => 'id',
+                        'id' => true,
+                        'type' => 'integer',
+                    ],
+                    [
+                        'fieldName' => 'name',
+                        'columnName' => 'title',
+                        'id' => false,
+                        'type' => 'string',
+                        'length' => 60,
+                        'unique' => true,
+                    ]
+                ],
+                'associations' => [
+                    [
+                        'type' => ClassMetadata::ONE_TO_MANY,
+                        'fieldName' => 'comments',
+                        'targetEntity' => Comment::class,
+                        'mappedBy' => 'post',
+                    ]
+                ]
+            ],
+        ];
+    }
+
+    /**
+     * @param DriverInterface $driver
+     * @param CacheItemPoolInterface $cache
+     * @return ClassMetadataFactory
+     */
     private function createClassMetadataFactory($driver = null, $cache = null)
     {
+        if(!$driver) {
+            $driver = new ArrayDriver($this->getFixturesData());
+        }
+        if(!$cache) {
+            $cache = new VoidCachePool();
+        }
         $factory = new ClassMetadataFactory($driver, $cache);
         return $factory;
     }
 
     /**
-     * Tests constructor
+     *
      */
-    public function testGetMetadataFor()
+    public function _testGetMetadataFor()
     {
-        $driver = $this->createMock(DriverInterface::class);
-        $cache = $this->createMock(CacheItemPoolInterface::class);
-        $factory = $this->createClassMetadataFactory($driver, $cache);
+        $factory = $this->createClassMetadataFactory();
         $metadata = $factory->getMetadataFor(Post::class);
         $this->assertNotEmpty($metadata);
         $this->assertTrue($metadata instanceof ClassMetadata);
+    }
+
+    /**
+     *
+     */
+    public function testGetMetadataForUndefinedClass()
+    {
+        $this->expectException(MappingException::class);
+        $factory = $this->createClassMetadataFactory();
+        $factory->getMetadataFor(Post::class.'invalid_class');
     }
 }
