3cf1b0e12db8f89946fca352ec21932015ba8255
Saidou GUEYE <saidou.gueye@gmail.com>
2018-04-17 15:44:41 -0400

    Remove handlesClass method from driver interface, add 2 more drivers

---
+++ tests/Mapping/AnnotationDriverTest.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: seydu
+ * Date: 3/20/18
+ * Time: 2:06 PM
+ */
+
+namespace Seydu\EloquentMetadata\Tests;
+
+
+use PHPUnit\Framework\TestCase;
+use Psr\Cache\CacheItemPoolInterface;
+use Seydu\EloquentMetadata\Mapping\ArrayDriver;
+use Seydu\EloquentMetadata\Mapping\ClassMetadata;
+use Seydu\EloquentMetadata\Mapping\ClassMetadataFactory;
+use Seydu\EloquentMetadata\Mapping\DriverInterface;
+use Seydu\EloquentMetadata\Tests\Models\Comment;
+use Seydu\EloquentMetadata\Tests\Models\Model;
+use Seydu\EloquentMetadata\Tests\Models\Post;
+
+class ArrayDriverTest extends TestCase
+{
+    public function testHandleClass()
+    {
+        $driver = new ArrayDriver([
+            Post::class => [],
+        ]);
+        $this->assertTrue($driver->handlesClass(Post::class));
+        $this->assertFalse($driver->handlesClass(Comment::class));
+    }
+
+    /**
+     * Tests constructor
+     */
+    public function testGetAllClassNames()
+    {
+        $driver = new ArrayDriver([
+            Post::class => [],
+            Comment::class => [],
+            Model::class => []
+        ]);
+        $classNames = $driver->getAllClassNames();
+        $this->assertInternalType('array', $classNames);
+        $this->assertCount(3    , $classNames);
+        $this->assertContains(Post::class, $classNames);
+        $this->assertContains(Comment::class, $classNames);
+        $this->assertContains(Model::class, $classNames);
+    }
+
+    /**
+     * Tests constructor
+     */
+    public function testGetMetadataForClass()
+    {
+        $driver = new ArrayDriver([
+            Post::class => [
+                'table' => 'post',
+                'fields' => [
+                    [
+                        'fieldName' => 'id',
+                        'columnName' => 'id',
+                        'id' => true,
+                        'type' => 'integer',
+                    ],
+                    [
+                        'fieldName' => 'name',
+                        'columnName' => 'title',
+                        'id' => false,
+                        'type' => 'string',
+                        'length' => 60,
+                        'unique' => true,
+                    ]
+                ],
+                'associations' => [
+                    [
+                        'type' => ClassMetadata::ONE_TO_MANY,
+                        'fieldName' => 'comments',
+                        'targetEntity' => Comment::class,
+                        'mappedBy' => 'post',
+                    ]
+                ]
+            ],
+        ]);
+        $metadata = new ClassMetadata(Post::class);
+        $driver->loadMetadataForClass(Post::class, $metadata);
+        $this->assertEquals('post', $metadata->getTableName());
+        $idMapping = $metadata->getFieldMapping('id');
+        $this->assertEquals('id', $idMapping['fieldName']);
+        $this->assertArraySubset(
+            [
+                'fieldName' => 'id',
+                'columnName' => 'id',
+                'id' => true,
+                'type' => 'integer',
+            ],
+            $idMapping
+        );
+        $fieldNames = $metadata->getFieldNames();
+        $this->assertCount(2, $fieldNames);
+        $this->assertArraySubset(['id', 'name'], $fieldNames);
+
+        $commentsMapping = $metadata->getAssociationMapping('comments');
+        $this->assertInternalType('array', $commentsMapping);
+        $this->assertArraySubset(
+            [
+                'type' => ClassMetadata::ONE_TO_MANY,
+                'fieldName' => 'comments',
+                'targetEntity' => Comment::class,
+                'mappedBy' => 'post',
+            ],
+            $commentsMapping
+        );
+        $associationNames = $metadata->getAssociationNames();
+        $this->assertInternalType('array', $associationNames);
+        $this->assertCount(1, $associationNames);
+        $this->assertArraySubset(['comments'], $associationNames);
+    }
+}
